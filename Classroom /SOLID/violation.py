dfc358467d01b5582d06c955c1bc0df29043745b
Ghanashyam Parajuli <prashant.ghanu2004@gmail.com>
2026-06-12 15:28:46 +0545

    Update violation.py

--- Classroom /SOLID/violation.py
+++ Classroom /SOLID/violation.py
@@ -5,7 +5,8 @@ Here is a discount system; every time you add a new customer_type shows up,
 we need to edit the same function.
 
 Note: Use of if-else statements is a common sign of OCP violation, 
-as it often indicates that the code is not designed to accommodate new functionality without modification.
+as it often indicates that the code is not designed to accommodate 
+new functionality without modification.
 """
 
 def calculate_price(price,customer_type):
@@ -16,7 +17,10 @@ def calculate_price(price,customer_type):
     elif customer_type == "vip":
         return price * 0.5
     
-# Consider, a new coustomer type: "first_timer" is added, we need to add another elif clause and edit the same function again, which violates OCP.
+''' Consider, a new coustomer type: "first_timer" is added, 
+we need to add another elif clause and edit the same function again, which violates OCP.
+'''
+
 '''
     elif customer_type == "first_timer":
         return price 
